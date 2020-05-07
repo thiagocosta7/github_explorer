2dd84c0630d4ab4f933082895317e1b9122a6449
Thiago Costa <thiago.costa@tcsystem.com.br>
2020-05-07 04:29:41 -0300

    Updates

---
+++ precache-manifest.658a6ac5d8fec448c6530ab32fec537b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8de77bc6f87e4fbf61583b5947dc926",
+    "url": "/github_explorer/index.html"
+  },
+  {
+    "revision": "a2103d31bffd66447aa8",
+    "url": "/github_explorer/static/js/2.ea415143.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/github_explorer/static/js/2.ea415143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2567af4d209dc604477c",
+    "url": "/github_explorer/static/js/main.f82ce492.chunk.js"
+  },
+  {
+    "revision": "c933c8565b327be3f4c4",
+    "url": "/github_explorer/static/js/runtime-main.de63d7c6.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github_explorer/static/media/github_bg.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github_explorer/static/media/logo.04f6798f.svg"
+  }
+]);
